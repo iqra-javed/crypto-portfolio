5ac694d2c25d26a512c9e2b531d051763bd0b3fe
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-06-23 13:29:21 -0500

    Create calculate_value action for Currency

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,5 +1,9 @@
 class Currency < ApplicationRecord
     COINMARKETCAP_API_KEY = ENV['coinmarketcap_api_key']
+    def calculate_value(amount)
+        (current_price.to_f * amount.to_f).round(4)
+    end
+
     def current_price
         url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug='
         request = HTTParty.get(url + self.slug, :headers => {"X-CMC_PRO_API_KEY" => COINMARKETCAP_API_KEY})
