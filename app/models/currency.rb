d2076c927d2d837344f67cf5eb67dafc2356c11a
iqra-javed <35740252+iqra-javed@users.noreply.github.com>
2020-06-23 13:20:16 -0500

    Create currencies controller, view and current_price action

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,8 @@
 class Currency < ApplicationRecord
+    COINMARKETCAP_API_KEY = ENV['coinmarketcap_api_key']
+    def current_price
+        url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug='
+        request = HTTParty.get(url + self.slug, :headers => {"X-CMC_PRO_API_KEY" => COINMARKETCAP_API_KEY})
+        response = JSON.parse(request.body)["data"]["1"]["quote"]["USD"]["price"]
+    end
 end
